f1ceae91748f010f1b651145b8e708ef9f74bb9e
mk <mk.eng1119@gmail.com>
2025-02-27 16:47:26 +0900

    style: fix font-size

--- src/components/ui/Typography/index.tsx
+++ src/components/ui/Typography/index.tsx
@@ -12,9 +12,9 @@ const TypographyVariants = cva([], {
   variants: {
     variant: {
       h1: 'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl',
-      h2: 'scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0',
-      h3: 'scroll-m-20 text-2xl font-semibold tracking-tight',
-      h4: 'scroll-m-20 text-xl font-semibold tracking-tight',
+      h2: 'scroll-m-20 pb-2 text-2xl font-semibold tracking-tight transition-colors first:mt-0 lg:text-3xl',
+      h3: 'scroll-m-20 text-lg font-semibold tracking-tight lg:text-xl',
+      h4: 'scroll-m-20 text-base font-semibold tracking-tight lg:text-xl',
       p: 'leading-7 [&:not(:first-child)]:mt-6',
       blockquote: 'mt-6 border-l-2 pl-6 italic',
     },
